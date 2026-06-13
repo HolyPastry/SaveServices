b339a83e55cf3597f748ada8d9f4b3e2722d54e2
Sebastien Berton <sebasti1.berton@gmail.com>
2026-06-13 11:06:07 +0900

    Updated Bakery Core

--- Test/Scripts/SaveSystemUnitTest.cs
+++ Test/Scripts/SaveSystemUnitTest.cs
@@ -1,38 +1,38 @@
 using System.Collections;
-using Holypastry.Bakery;
+//using Holypastry.Bakery;
 using UnityEngine;
 namespace Bakery.Saves.Test
 {
-    public class SaveSystemUnitTest : MonoBehaviour
-    {
-        [SerializeField] private TestSO _testSO;
+    // public class SaveSystemUnitTest : MonoBehaviour
+    // {
+    //     [SerializeField] private TestSO _testSO;
 
-        private DataCollection<TestSO> _collection;
+    //     private DataCollection<TestSO> _collection;
 
-        void Awake()
-        {
-            _collection = new DataCollection<TestSO>("Tests");
-        }
+    //     void Awake()
+    //     {
+    //         _collection = new DataCollection<TestSO>("Tests");
+    //     }
 
-        IEnumerator Start()
-        {
-            var data = new TestSaveData
-            {
-                TestSO = _testSO
-            };
+    //     IEnumerator Start()
+    //     {
+    //         var data = new TestSaveData
+    //         {
+    //             TestSO = _testSO
+    //         };
 
-            SaveServices.DeleteSave();
-            SaveServices.Save("Key", data);
+    //         SaveServices.DeleteSave();
+    //         SaveServices.Save("Key", data);
 
-            yield return new WaitForSeconds(1f);
+    //         yield return new WaitForSeconds(1f);
 
-            var loadedData = SaveServices.Load<TestSaveData>("Key");
-            loadedData.TestSO = _collection.GetFromName(loadedData.TestSOName);
+    //         var loadedData = SaveServices.Load<TestSaveData>("Key");
+    //         loadedData.TestSO = _collection.GetFromName(loadedData.TestSOName);
 
-            Debug.Log(loadedData.TestString);
-            Debug.Log(loadedData.TestInt);
-            Debug.Log(loadedData.TestList.Count);
-            Debug.Log(loadedData.TestSO.TestSOString);
-        }
-    }
+    //         Debug.Log(loadedData.TestString);
+    //         Debug.Log(loadedData.TestInt);
+    //         Debug.Log(loadedData.TestList.Count);
+    //         Debug.Log(loadedData.TestSO.TestSOString);
+    //     }
+    // }
 }
